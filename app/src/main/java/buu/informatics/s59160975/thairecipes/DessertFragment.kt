de5cc88d41da6dfbab8e41f309e8547b7a53ec16
Ramnaree Mailaem <59160975@go.buu.ac.th>
2019-11-21 02:09:05 +0700

    Finish

--- app/src/main/java/buu/informatics/s59160975/thairecipes/DessertFragment.kt
+++ app/src/main/java/buu/informatics/s59160975/thairecipes/DessertFragment.kt
@@ -1,13 +1,13 @@
 package buu.informatics.s59160975.thairecipes
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.squareup.picasso.Picasso
 
 /**
@@ -15,11 +15,14 @@ import com.squareup.picasso.Picasso
  */
 class DessertFragment : Fragment() {
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_dessert, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -32,6 +35,27 @@ class DessertFragment : Fragment() {
         Picasso.get().load(args.image).into(imageMenu)
         detailMenu.text = args.detail
     }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun getShareIntent() : Intent {
+        Toast.makeText(activity, "You select share", Toast.LENGTH_SHORT).show()
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, "ฉันกำลังเล่นแอปนี้อยู่!")
+        return shareIntent
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
 
 
 }
--- app/src/main/java/buu/informatics/s59160975/thairecipes/MaincoursesFragment.kt
+++ app/src/main/java/buu/informatics/s59160975/thairecipes/MaincoursesFragment.kt
@@ -1,13 +1,13 @@
 package buu.informatics.s59160975.thairecipes
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.squareup.picasso.Picasso
 
 /**
@@ -20,6 +20,7 @@ class MaincoursesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_maincourses, container, false)
     }
 
@@ -32,6 +33,26 @@ class MaincoursesFragment : Fragment() {
         nameMenu.text = args.name
         Picasso.get().load(args.Image).into(imageMenu)
         detailMenu.text = args.detail
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item!!.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun getShareIntent() : Intent {
+        Toast.makeText(activity, "You select share", Toast.LENGTH_SHORT).show()
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, "ฉันกำลังเล่นแอปนี้อยู่!")
+        return shareIntent
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
     }
 
 
